55f7f8c5ad0c98894da4dc7e55ab1dd7accc42df
Cat Hanbit <changwoon4@naver.com>
2022-07-05 16:49:48 +0900

    test

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+  {
+    name: "이인제",
+    comment: "안녕하세요, 소플입니다.",
+  },
+  {
+    name: "강민경",
+    comment: "헬로우",
+  },
+  {
+    name: "이해리",
+    comment: "하이에브리원",
+  },
+];
+
 function CommentList(props) {
   return (
     <div>
-      <Comment></Comment>
+      {comments.map((comment) => {
+        return (
+          <Comment name={comment.name} comment={comment.comment}></Comment>
+        );
+      })}
     </div>
   );
 }
