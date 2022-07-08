a6da12568e58c6a084fb776be863e6e1b593263e
Cat Hanbit <changwoon4@naver.com>
2022-07-08 17:58:20 +0900

    save

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import NotificationList from "./chapter_06/NotificationList";
 import Accommodate from "./chapter_07/Accommodate,";
 import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
+import SignUp from "./chapter_11/SignUp";
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -50,7 +52,7 @@ import LandingPage from "./chapter_09/LandingPage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage></LandingPage>
+    <SignUp></SignUp>
   </React.StrictMode>,
   document.getElementById("root")
 );
